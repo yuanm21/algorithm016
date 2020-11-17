b5d1f376c2d10f9704d53cbe4aa89c6dca51db7e
袁满 <yuanm21@163.com>
2020-11-18 00:08:29 +0800

    Add files via upload

---
+++ Week_09/63.不同路径-ii.cpp
@@ -0,0 +1,31 @@
+/*
+ * @lc app=leetcode.cn id=63 lang=cpp
+ *
+ * [63] 不同路径 II
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        int n = obstacleGrid.size(), m = obstacleGrid.at(0).size();
+        vector <int> f(m);
+
+        f[0] = (obstacleGrid[0][0] == 0);
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < m; ++j) {
+                if (obstacleGrid[i][j] == 1) {
+                    f[j] = 0;
+                    continue;
+                }
+                if (j - 1 >= 0 && obstacleGrid[i][j - 1] == 0) {
+                    f[j] += f[j - 1];
+                }
+            }
+        }
+
+        return f.back();
+    }
+};
+// @lc code=end
+
